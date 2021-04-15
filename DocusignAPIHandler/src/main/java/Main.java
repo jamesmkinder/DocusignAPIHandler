f4beb4672736e0ad0f9aee4e55d4c0606f83b798
jmkgfk <jmkgfk@gmail.com>
2021-04-15 09:50:51 -0400

    Testing args passthrough to java

--- DocusignAPIHandler/src/main/java/Main.java
+++ DocusignAPIHandler/src/main/java/Main.java
@@ -1,12 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws SQLException {
 
+        Connection conn = DriverManager.getConnection("jdbc:ucanaccess://G://codeRepository//WeldLoggerPro Project//WeldMaterialControlLog_be.accdb");
 
+        ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM WELD_JOB WHERE JOB_ID = " + args[0]);
+        rs.next();
 
+        System.out.println(rs.getString("WELDER_CLOCK_NUM"));
 
-
-
-
+        conn.close();
     }
 }
